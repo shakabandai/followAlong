4423e9c786f24cdfb009f660b581c95d4bbe0a6f
Justin Daniel <justindaniel@Justins-MBP.lan1>
2018-07-20 14:25:09 -0500

    added stylesheet

--- app.js
+++ app.js
@@ -8,9 +8,25 @@ $(document).ready(function(){
 	// });
 
 	$('.store-btn').on("click", function(event){
-	  	localStorage.setItem('hrext', 'three is the best');
+	  	let titleValue = $('.input-field-title').val();
+	  	let contentValue = $('.input-field-body').val();
+	  	
+	  	localStorage.setItem('.titleValue', titleValue);
+	  	localStorage.setItem('.contentValue', contentValue);
+   
 	});
+
 	$('.get-btn').on("click", function(event){
-	  	console.log(localStorage.getItem('hrext'));
+	  	let titleValue = localStorage.getItem('.titleValue');
+	  	let contentValue = localStorage.getItem('.contentValue');
+
+	  	$('.debug').html(`<p>$(contentValue) $(contentValue)</p>`);
+	});
+
+	$('.delete-btn').on("click", function(event){
+	  	localStorage.removeItem('.titleValue');
+	  	localStorage.removeItem('contentValue');
+
+	  	$('.debug').html(`<p>Items Deleted</p>`);
 	});
 });
